14b6bf372447efd63f64cbab43d70cc1fae1634d
Kurdiamina3 <kurdiamina3@gmail.com>
2021-12-08 21:01:34 -0500

    add reply

--- models/Pizza.js
+++ models/Pizza.js
@@ -3,20 +3,26 @@ const dateFormat = require('../utils/dateFormat');
 
 const PizzaSchema = new Schema({
     pizzaName: {
-      type: String
-    },
-    createdBy: {
-      type: String
-    },
+        type: String,
+        required: true,
+        trim: true
+      },
+      createdBy: {
+        type: String,
+        required: true,
+        trim: true
+      },
     createdAt: {
         type: Date,
         default: Date.now,
         get: (createdAtVal) => dateFormat(createdAtVal)
     },
     size: {
-      type: String,
-      default: 'Large'
-    },
+        type: String,
+        required: true,
+        enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
+        default: 'Large'
+      },
     toppings: [],
     comments: [
         {
@@ -35,7 +41,7 @@ const PizzaSchema = new Schema({
 
 // get total count of comments and replies on retrieval
 PizzaSchema.virtual('commentCount').get(function() {
-    return this.comments.length;
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
   });
 
   // create the Pizza model using the PizzaSchema
